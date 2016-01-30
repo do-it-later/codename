fb9aca1f9a4cbdc28209daba68849dcf8f4da3f2
Joseph Tran <valrigardian@gmail.com>
2016-01-30 12:33:19 +1100

    Add Team, set project settings to only allow 16:9

---
+++ Assets/Scripts/Players/Team.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Team {
+
+    private int score;
+
+    public Team()
+    {
+        score = 0;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public int ModifyScore(int modifyAmount)
+    {
+        score += modifyAmount;
+    }
+
+}
